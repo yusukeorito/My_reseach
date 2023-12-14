28e3121209b115542e990cb19dd86bd3348acefe
織戸悠輔 <oritoyusuke@oridoyuusukenoMacBook-Air.local>
2023-12-14 11:03:10 +0900

    test commit for Utils

---
+++ Utils/get_q.py
@@ -0,0 +1,170 @@
+#overlapの計算に用いる関数
+import numpy as np
+import pickle
+import joblib
+from tqdm import tqdm
+
+import tensorflow as tf
+from tensorflow.keras.callbacks import Callback,ModelCheckpoint
+
+
+
+
+class LogEpochIntermediateCallcack(Callback):
+  def __init__(self, layer_name_list,CFG,X_train,path):
+    self.layer_name_list = layer_name_list
+    self.spin_dict = {key: [] for key in self.layer_name_list}
+    #self.spin_dict_test = {key: [] for key in self.layer_name_list}
+    self.nextMeas=1
+    self.CFG = CFG
+    self.X_train = X_train
+    self.path = path
+    
+
+
+  def on_train_begin(self, batch, logs=None):
+    self.spin_dict['time'] = [0]
+    #self.spin_dict_test['time'] = [0]
+    for l in self.layer_name_list:
+      intermediate_layer_model = tf.keras.Model(inputs=self.model.input,outputs=self.model.get_layer(l).output)
+      if self.CFG['M'] == 60000:
+        intermediate_output = intermediate_layer_model.predict(X_train_)
+      else:
+        intermediate_output = intermediate_layer_model.predict(self.X_train)
+      #intermediate_output_test = intermediate_layer_model.predict(test_in)
+      tf.keras.backend.clear_session()
+      self.spin_dict[l].append(intermediate_output)
+      #self.spin_dict_test[l].append(intermediate_output_test)
+
+  def on_epoch_begin(self, epoch, logs=None):
+      self.ep = epoch
+
+  def on_epoch_end(self, batch, logs):
+      if self.ep+1==self.nextMeas:
+        for l in self.layer_name_list:
+          intermediate_layer_model = tf.keras.Model(inputs=self.model.input,outputs=self.model.get_layer(l).output)
+          if self.CFG['M'] == 60000:
+            intermediate_output = intermediate_layer_model.predict(X_train_)
+          else:
+            intermediate_output = intermediate_layer_model.predict(self.X_train)
+          #intermediate_output_test = intermediate_layer_model.predict(test_in)
+          tf.keras.backend.clear_session()
+          self.spin_dict[l].append(intermediate_output)
+          #self.spin_dict_test[l].append(intermediate_output_test)
+        self.spin_dict['time']+=[self.ep+1]
+        #self.spin_dict_test['time']+=[self.ep+1]
+        self.nextMeas=int(self.nextMeas*1.1)
+        if self.ep+1==self.nextMeas:
+          self.nextMeas = self.nextMeas+1
+
+
+  def on_train_end(self, logs=None):
+    for l in self.layer_name_list:
+      self.spin_dict[l] = np.array(self.spin_dict[l])
+      #self.spin_dict_test[l] = np.array(self.spin_dict_test[l])
+    joblib.dump(self.spin_dict,self.path)
+    #with open(f'./Output/Spin/M600/model{self.model_num}_M{CFG.M}_L{CFG.L}_A{CFG.A}_test.txt','wb') as handle:
+        #pickle.dump(self.spin_dict_test, handle)
+
+
+
+def get_normalized_spin(SpinA, SpinB):
+  spinA_norm = SpinA.copy()
+  spinB_norm = SpinB.copy()
+  for l in tqdm(CFG.layer_name_list):
+        squared_sum_A = np.sum(SpinA[l]**2, axis=2)
+        squared_sum_B = np.sum(SpinB[l]**2, axis=2)
+        # 規格化定数を計算
+        normalization_constA = np.sqrt(100 / squared_sum_A)
+        normalization_constB = np.sqrt(100 / squared_sum_B)
+        # 規格化した配列を計算
+        spinA_norm[l] = SpinA[l] * normalization_constA[:, :, np.newaxis]
+        spinB_norm[l] = SpinB[l] * normalization_constB[:, :, np.newaxis]
+
+  return spinA_norm, spinB_norm
+
+
+def calc_q_(A: np.ndarray, B: np.ndarray) -> float:
+    M, N = A.shape
+    dot_product = np.dot(A.T, B)
+    x = np.sum(dot_product ** 2)
+    x /= N * M * M
+    x -= N / M
+    return x
+
+def calc_sim_q(A: np.ndarray, B: np.ndarray) -> float:
+  mean = A * B
+  sim_q = np.mean(mean)
+  return sim_q
+
+
+def get_sim_q(spinA, spinB, layer_name_list, path):
+  qab_dict={'time':spinA['time']}#時刻の初期化
+  qaa_dict={'time':spinA['time']}
+  sim_q_dict={'time':spinA['time']}
+  for l in tqdm(layer_name_list):
+      qab_list=[]
+      qaa_list=[]
+      sim_q_list=[]
+      for i in range(len(spinA[l])):
+          ab = calc_sim_q(spinA[l][i],spinB[l][i])
+          aa = calc_sim_q(spinA[l][i],spinA[l][i])
+          bb = calc_sim_q(spinB[l][i],spinB[l][i])
+          sim_q = ab/(np.sqrt(aa)*np.sqrt(bb))
+          qab_list.append(ab)
+          qaa_list.append(aa)
+          sim_q_list.append(sim_q)
+      qab_dict[l] = qab_list
+      qaa_dict[l] = qaa_list
+      sim_q_dict[l] = sim_q_list
+  joblib.dump(sim_q_dict, path)
+  return sim_q_dict
+
+
+def get_q2(spinA, spinB, layer_name_list ,path):
+    qab_dict={'time':spinA['time']}#時刻の初期化
+    qaa_dict={'time':spinA['time']}
+    q2_dict={'time':spinA['time']}
+    for l in tqdm(layer_name_list):
+        qab_list=[]
+        qaa_list=[]
+        q2_list=[]
+        for i in range(len(spinA[l])):
+            ab = calc_q_(spinA[l][i],spinB[l][i])
+            aa= calc_q_(spinA[l][i],spinA[l][i])
+            bb = calc_q_(spinB[l][i],spinB[l][i])
+            q2 = ab/(np.sqrt(aa)*np.sqrt(bb))
+            qab_list.append(ab)
+            qaa_list.append(aa)
+            q2_list.append(q2)
+        qab_dict[l] = qab_list
+        qaa_dict[l] = qaa_list
+        q2_dict[l] = q2_list
+    """
+    with open(f'./Output/Overlap/q/{CFG.alg}_qab_norm_M{CFG.M}_L{CFG.L}_A{CFG.A}_{CFG.train}.txt','wb') as handle:
+        pickle.dump(qab_dict, handle)
+    with open(f'./Output/Overlap/q/{CFG.alg}_qaa_norm_M{CFG.M}_L{CFG.L}_A{CFG.A}_{CFG.train}.txt','wb') as handle:
+        pickle.dump(qaa_dict, handle)
+    """
+    joblib.dump(q2_dict, path)
+    return qab_dict, qaa_dict, q2_dict
+
+def get_layer_overlap(qab:dict,qaa:dict,q2:dict,sim_q:dict, layer_name_list, path):
+  layer_dict={}
+  layer_q2=[]
+  layer_qab=[]
+  layer_qaa=[]
+  layer_sim_q=[]
+
+  for i, l in enumerate(layer_name_list):
+      layer_q2.append(q2[l][-1])#平衡状態のOverlapを取得
+      layer_qab.append(qab[l][-1])
+      layer_qaa.append(qaa[l][-1])
+      layer_sim_q.append(sim_q[l][-1])
+  layer_dict['q2']=layer_q2
+  layer_dict['qab']=layer_qab
+  layer_dict['qaa']=layer_qaa
+  layer_dict['sim_q']=layer_sim_q
+
+  joblib.dump(layer_dict, path)
+  return layer_dict
---
+++ Utils/utils.py
@@ -0,0 +1,67 @@
+#汎用的に利用できる関数
+import os
+import random
+
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+import tensorflow as tf
+import tensorflow.keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras.callbacks import Callback,ModelCheckpoint
+from tensorflow.keras.layers import Dense,BatchNormalization,Activation
+from tensorflow.keras.activations import relu
+from tensorflow.keras.utils import plot_model
+
+
+#seedの固定
+def set_seed(seed):
+    tf.random.set_seed(seed)
+    # optional
+    # for numpy.random
+    np.random.seed(seed)
+    # for built-in random
+    random.seed(seed)
+    # for hash seed
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    
+
+def load_mnist_data(mnist_file_path):
+    with np.load(mnist_file_path) as data:
+        X_train, y_train = data['x_train'], data['y_train']
+        X_test, y_test = data['x_test'], data['y_test']
+    
+    return X_train, y_train, X_test, y_test
+    
+    
+#データの前処理(標準化とデータ型)
+def preprocess_data(X_train_, y_train_):
+    # Rasterize and normalize samples
+    X_train_ = X_train_.reshape(X_train_.shape[0], -1)
+    y_train_ = y_train_.reshape(y_train_.shape[0], -1)
+
+    X_train_ = X_train_ / 255
+    y_train_ = y_train_ / 255
+    # Use 32-bit instead of 64-bit float
+    X_train_ = X_train_.astype("float32")
+    y_train_ = y_train_.astype("float32")
+
+    return X_train_, y_train_
+
+def create_fully_model(params:dict, W, B):
+    model=Sequential(name='fully_connect_model')
+    model.add(Dense(params['N'], kernel_initializer=W,bias_initializer=B,input_shape=(params['N'],), name='Affine1',))
+    model.add(BatchNormalization(name='batch_normalization1'))
+    model.add(Activation('relu', name='activation1'))
+    for i in range(1, params['num_layers']-1):
+        model.add(Dense(params['N'], kernel_initializer=W,bias_initializer=B,name=f'Affine{i+1}'))
+        model.add(BatchNormalization(name=f'batch_normalization{i+1}'))
+        model.add(Activation(params['activation'],name=f'activation{i+1}'))
+        
+    model.add(Dense(params['num_classes'],kernel_initializer=W,bias_initializer=B,name='output',))
+    model.add(BatchNormalization(name=f'batch_normalization{params["num_layers"]}'))
+    model.add(Activation('softmax', name=f'activation{params["num_layers"]}'))
+    model.compile(loss=params['loss'], optimizer=params['optimizer'],metrics=[params['metric']])
+    return model
